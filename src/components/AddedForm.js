7c7241d357c544083204a1085b7b789b30295d67
Alex Brln <biryulin.sanya@icloud.com>
2021-11-27 00:12:22 +0300

    Modified the add button. Added an icon for closing/deleting the form

--- src/components/AddedForm.js
+++ src/components/AddedForm.js
@@ -1,24 +1,69 @@
 import React from 'react'
 import '../styles/AddedForm.css'
-const AddedForm = () => {
-    return (
-        <div className='add'>
-            <form action="" className="add-form">
-                <button className='add-btn' onClick={(e) => e.preventDefault()} >+</button>
-                <h3>Новая доска</h3>
-            </form>
-            <div className='addedForm hide' style={{ width: '300px', height: '150px', background: '#fff', borderRadius: '5px', boxShadow: '0px 4px 8px -1px rgba(34, 60, 80, 0.2)' }}>
-                <form action="" style={{ textAlign: 'center' }}>
-                    <label for="" style={{ display: 'block', textAlign: 'left', padding: '10px 10%' }}> Название доски</label>
-                    <input type="text" placeholder='Моя доска' />
+class AddedForm extends React.Component {
+
+    state = {
+        formOpen: false,
+        text: '',
+    }
+
+    hideForm = (e) => {
+        this.state.formOpen ? this.setState({ formOpen: false }) : this.setState({ formOpen: true })
+        const form = document.querySelector('.add-form')
+        e.preventDefault()
+    }
+    renderDeleteForm = () => {
+        return (
+            <div className='delete-form'>
+                <a href=""><h3>+</h3></a>
+            </div>
+        )
+    }
+    renderAddButton = () => {
+        const { list } = this.props;
+
+        return (
+            <button className="ui positive basic button add-form-hide-btn">Добавить</button>
+        )
+    }
+    handleInputChange = (e) => {
+        this.setState({
+            text: e.target.value,
+        })
+
+    }
+
+    renderAddedForm = () => {
+        return (
+            <div className='add-form-hide '>
+                <form action="">
+                    <label for="" > Название доски</label>
+                    <input type="text"
+                        placeholder='Моя доска'
+                        value={this.state.text}
+                        onChange={this.handleInputChange} />
                     <div style={{ marginTop: '10%' }}>
                         <button className="ui secondary basic button">Отмена</button>
-                        <button className="ui positive basic button">Сохранить</button>
+                        {this.renderAddButton()}
                     </div>
                 </form>
             </div>
-        </div>
-    )
+        )
+    }
+
+    render() {
+        return (
+            <div className='add'>
+                <form action="" className="add-form">
+                    <button className='add-btn' onClick={this.hideForm} >+</button>
+                    <a href="" onClick={this.hideForm}><h3>Новая доска</h3></a>
+                    {this.state.formOpen ? this.renderDeleteForm() : console.log('Форма скрыта')}
+
+                </form>
+                {this.state.formOpen ? this.renderAddedForm() : console.log('Форма скрыта')}
+            </div>
+        )
+    }
 }
 
 export default AddedForm
